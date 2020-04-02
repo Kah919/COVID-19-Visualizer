be36ac37f3ad6aba8b5e4095b29848ad710284fa
Kah919 <kahyap13@gmail.com>
2020-04-02 14:37:02 -0400

    react hook set up

--- covid-19-visualizer/src/components/USAMap.js
+++ covid-19-visualizer/src/components/USAMap.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import USAMap from "react-usa-map";
  
-class App extends Component {
+const Map = () => {
+    const [ states, setStates ] = useState([])
+
   /* mandatory */
-  mapHandler = (event) => {
+  const mapHandler = (event) => {
     alert(event.target.dataset.name);
   };
  
   /* optional customization of filling per state and calling custom callbacks per state */
-  statesCustomConfig = () => {
+
+  useEffect(() => {
+    fetch("https://covidtracking.com/api/states")
+    .then(res => res.json())
+    .then(data => {
+        setStates(data)
+    })
+  })
+
+  const statesCustomConfig = () => {
     return {
       "NJ": {
         fill: "navy",
         clickHandler: (event) => console.log('Custom handler for NJ', event.target.dataset)
       },
       "NY": {
-        fill: "#CC0000"
+        fill: "#CC0000",
+        clickHandler: (event) => console.log(states)
       }
     };
   };
  
-  render() {
+    
     return (
       <div className="App">
-        <USAMap customize={this.statesCustomConfig()} onClick={this.mapHandler} />
+        <USAMap customize={statesCustomConfig()} onClick={mapHandler} />
       </div>
     );
-  }
+  
 }
  
-export default App;
+export default Map;
