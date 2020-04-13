d7a3311bcdde4e06e28e12acbf5595ba47bd9ddd
Kah919 <kahyap13@gmail.com>
2020-04-13 17:17:08 -0400

    adjust data container for better visibility

--- covid-19-visualizer/src/components/Legend.js
+++ covid-19-visualizer/src/components/Legend.js
@@ -4,13 +4,22 @@ import styled from 'styled-components';
 export default props => {
     const Div = styled.div`
         border: 1px solid black;
+        width: 30%;
+        border-radius: 10px;
+    `;
+
+    const H1 = styled.h1`
+        font-size: 1.2rem;
+    `;
+
+    const P = styled.p`
+        text-align: left;
     `;
 
     return (
         <Div>
-            <h1> This map indicates the severity of COVID-19 </h1>
-            <p> The brighter the state, the more cases there are. </p>
-            <p> The state in red is currently the state with the highest COVID-19 cases </p>
+            <H1> This map indicates the severity of COVID-19 </H1>
+            <P> The brighter the state, the more cases there are. The state in red is currently the state with the highest COVID-19 cases </P>
         </Div>
     )
 }
--- covid-19-visualizer/src/components/StateTimeLine.js
+++ covid-19-visualizer/src/components/StateTimeLine.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import CoronaVirusImg from '../images/coronavirus.png'
+import CoronaVirusImg from '../images/coronavirus.png';
+import Spinner from 'react-bootstrap/Spinner';
+import styled from 'styled-components';
 
 const StateTimeLine = props => {
+    const Div = styled.div`
+      width: 100%;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-evenly;
+    `;
+
     const dayElement = () => {
       if(props.stateDataTimeLine.length > 0) {
         return props.stateDataTimeLine.map(data => {
@@ -19,6 +28,18 @@ const StateTimeLine = props => {
                 </ul>
           </VerticalTimelineElement>
         })
+      } else {
+        return(
+          <Div>
+            <Spinner animation="grow" variant="primary" />
+            <Spinner animation="grow" variant="secondary" />
+            <Spinner animation="grow" variant="success" />
+            <Spinner animation="grow" variant="danger" />
+            <Spinner animation="grow" variant="warning" />
+            <Spinner animation="grow" variant="info" />
+            <Spinner animation="grow" variant="dark" />
+          </Div>
+        )
       }
     }
 
