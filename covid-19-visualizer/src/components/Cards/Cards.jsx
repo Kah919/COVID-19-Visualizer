6edd4fcfe5ffca299b854aa7d6cca7af4b78334a
Kah919 <kahyap13@gmail.com>
2020-04-26 15:49:00 -0400

    data number now increases from 0 to value when loaded

--- covid-19-visualizer/src/components/Cards/Cards.jsx
+++ covid-19-visualizer/src/components/Cards/Cards.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
 
-export default props => {
-    console.log(props)
+export default ({data: { confirmed, recovered, deaths, lastUpdate }}) => {
+    if(!confirmed) {
+        return 'Loading...'
+    }
     return(
-        <h1> Cards </h1>
+        <div className="global-container">
+            <Grid container spacing={3} justify="center">
+                <Grid item component={ Card }>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom> Infected </Typography>
+                        <Typography variant="h5"> 
+                            <CountUp
+                                start={ 0 }
+                                end={ confirmed.value }
+                                duration={ 2.5 }
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary"> Real Date </Typography>
+                        <Typography variant="body2"> Number of active cases of COVID-19 </Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={ Card }>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom> Recovered </Typography>
+                        <Typography variant="h5"> Real Data </Typography>
+                        <Typography color="textSecondary"> Real Date </Typography>
+                        <Typography variant="body2"> Number of recoveries COVID-19 </Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={ Card }>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom> Deaths </Typography>
+                        <Typography variant="h5"> Real Data </Typography>
+                        <Typography color="textSecondary"> Real Date </Typography>
+                        <Typography variant="body2"> Number of deaths caused by COVID-19 </Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
