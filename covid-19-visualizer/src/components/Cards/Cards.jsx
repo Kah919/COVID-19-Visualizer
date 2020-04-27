4d82ef6da223a630b2d790ffeee730c782b600fa
Kah919 <kahyap13@gmail.com>
2020-04-26 23:29:55 -0400

    resoolve chartjs import issues

--- covid-19-visualizer/src/components/Cards/Cards.jsx
+++ covid-19-visualizer/src/components/Cards/Cards.jsx
@@ -1,42 +1,43 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
+import styles from './Cards.module.css';
+import cx from 'classnames';
 
 export default ({data: { confirmed, recovered, deaths, lastUpdate }}) => {
     if(!confirmed) {
         return 'Loading...'
     }
     return(
-        <div className="global-container">
+        <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={ Card }>
+                <Grid item component={ Card } className={ cx(styles.card, styles.infected) } xs={ 12 } md={ 3 }>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom> Infected </Typography>
                         <Typography variant="h5"> 
-                            <CountUp
-                                start={ 0 }
-                                end={ confirmed.value }
-                                duration={ 2.5 }
-                                separator=","
-                            />
+                            <CountUp start={ 0 } end={ confirmed.value } duration={ 2.5 } separator="," />
                         </Typography>
-                        <Typography color="textSecondary"> Real Date </Typography>
+                        <Typography color="textSecondary"> { new Date(lastUpdate).toDateString() } </Typography>
                         <Typography variant="body2"> Number of active cases of COVID-19 </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={ Card }>
+                <Grid item component={ Card } className={ cx(styles.card, styles.recovered) } xs={ 12 } md={ 3 }>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom> Recovered </Typography>
-                        <Typography variant="h5"> Real Data </Typography>
-                        <Typography color="textSecondary"> Real Date </Typography>
+                        <Typography variant="h5"> 
+                            <CountUp start={ 0 } end={ recovered.value } duration={ 2.5 } separator="," />
+                        </Typography>
+                        <Typography color="textSecondary"> { new Date(lastUpdate).toDateString() } </Typography>
                         <Typography variant="body2"> Number of recoveries COVID-19 </Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={ Card }>
+                <Grid item component={ Card } className={ cx(styles.card, styles.deaths) } xs={ 12 } md={ 3 }>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom> Deaths </Typography>
-                        <Typography variant="h5"> Real Data </Typography>
-                        <Typography color="textSecondary"> Real Date </Typography>
+                        <Typography variant="h5"> 
+                            <CountUp start={ 0 } end={ deaths.value } duration={ 2.5 } separator="," />
+                        </Typography>
+                        <Typography color="textSecondary"> { new Date(lastUpdate).toDateString() } </Typography>
                         <Typography variant="body2"> Number of deaths caused by COVID-19 </Typography>
                     </CardContent>
                 </Grid>
--- covid-19-visualizer/src/components/Charts/Charts.jsx
+++ covid-19-visualizer/src/components/Charts/Charts.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../api';
+import { Line, Bar } from 'react-chartjs-2';
+import styles from './Charts.module.css';
 
 export default () => {
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async() => {
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchAPI();
+    })
+
+
+
     return(
         <h1> Charts </h1>
     )
