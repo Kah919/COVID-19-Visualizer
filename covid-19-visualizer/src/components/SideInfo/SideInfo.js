38678da998875cf167964cb0103770ee9736b0ae
Kah919 <kahyap13@gmail.com>
2020-05-01 14:49:11 -0400

    update caption

--- covid-19-visualizer/src/components/SideInfo/SideInfo.js
+++ covid-19-visualizer/src/components/SideInfo/SideInfo.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import styles from './SideInfo.module.css';
 import cx from 'classnames';
-
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 export default ({ states }) => {
     const sortStates = () => {
@@ -22,13 +28,23 @@ export default ({ states }) => {
         return stateObj;
     }
 
+    const mostCaption = state => {
+        const { name, positive } = state;
+        return <p> { `${name} is currently the leading state with the most COVID-19 cases at ${positive}.` } </p>
+    }
+
+    const leastCaption = state => {
+        const { name, positive } = state;
+        return <p> { `${name} is currently the state with the least amount of COVID-19 cases at ${positive}.` } </p>
+    }
+
 
     return (
         <div className="side-container">
             <Grid container spacing={3} justify="center">
                 <Grid item component={ Card } xs={ 12 } md={ 10 } className={ cx(styles.legend, styles.card) }>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom> Legend </Typography>
+                        <h1> Legend </h1>
                         <Typography variant="body2"> This map shows which state has the most COVID-19 cases based on the color. The brighter the green the more cases there are. The state colored in red is currently the state with the most COVID-19 cases. 
                         <br></br>
                         Clicking on a state will show you the amount of cases, recoveries, deaths, and timeline of that current state.
@@ -37,14 +53,14 @@ export default ({ states }) => {
                 </Grid>
                 <Grid item component={ Card } xs={ 12 } md={ 10 } className={ cx(styles.legend, styles.most) }>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom> Most Cases </Typography>
-                        <Typography variant="body2"> { states.length ? stateData(states.length - 1).name : null } </Typography>
+                        <h2> Most Cases </h2>
+                        <Typography variant="body2"> { states.length ? mostCaption(stateData(states.length - 1)) : null } </Typography>
                     </CardContent>
                 </Grid>
                 <Grid item component={ Card } xs={ 12 } md={ 10 } className={ cx(styles.legend, styles.least) }>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom> Least Cases </Typography>
-                        <Typography variant="body2"> { states.length ? stateData(0).name : null } </Typography>
+                        <h2> Least Cases </h2>
+                        <Typography variant="body2"> { states.length ? leastCaption(stateData(0)) : null } </Typography>
                     </CardContent>
                 </Grid>
             </Grid>
